e6512d873fb9a05a09e78dc60a39c5addf0dfa13
daratyoon <sandratsaiiyun@yahoo.com>
2023-02-01 22:28:38 -0600

    Imported moment.js and tz

--- src/script.js
+++ src/script.js
@@ -1,5 +1,18 @@
 function alertCityTime(event) {
-  alert(event.target.value);
+  if (event.target.value.lenght > 0) {
+    alert(event.target.value);
+  }
+  if (event.target.value === "houston") {
+    alert(`It is ${houstonTime} in Houston, Texas, USA`);
+  }
+
+  let houstonTime = moment()
+    .tz("America/Chicago")
+    .format("dddd MM Do, YYYY, H:mm A");
+  let seouldTime = moment().tz("Asia/Seoul").format("dddd MM Do, YYYY, H:mm A");
+  let taipeiTime = moment()
+    .tz("Asia/Taipei")
+    .format("dddd MM Do, YYYY, H:mm A");
 }
 
 let citiesSelectElement = document.querySelector("#cities-time");
