bc14982e7adb76fb847aab4efcc64b95abfb558d
daratyoon <sandratsaiiyun@yahoo.com>
2023-02-02 09:51:18 -0600

    Moved variables outside the function, formatted dates, fixed errors

--- src/script.js
+++ src/script.js
@@ -1,20 +1,25 @@
 function alertCityTime(event) {
-  if (event.target.value.lenght > 0) {
-    alert(event.target.value);
+  if (event.target.value.length > 0) {
+    if (event.target.value === "houston") {
+      alert(`It is ${houstonTime} in Houston, Texas, USA`);
+    }
+    if (event.target.value === "seoul") {
+      alert(`It is ${seoulTime} in Seoul, South Korea`);
+    }
+    if (event.target.value === "taipei") {
+      alert(`It is ${taipeiTime} in Taipei, Taiwan`);
+    }
   }
-  if (event.target.value === "houston") {
-    alert(`It is ${houstonTime} in Houston, Texas, USA`);
-  }
-
-  let houstonTime = moment()
-    .tz("America/Chicago")
-    .format("dddd MM Do, YYYY, H:mm A");
-  let seouldTime = moment().tz("Asia/Seoul").format("dddd MM Do, YYYY, H:mm A");
-  let taipeiTime = moment()
-    .tz("Asia/Taipei")
-    .format("dddd MM Do, YYYY, H:mm A");
 }
 
 let citiesSelectElement = document.querySelector("#cities-time");
 
 citiesSelectElement.addEventListener("change", alertCityTime);
+
+let houstonTime = moment()
+  .tz("America/Chicago")
+  .format("dddd, MMMM Do, YYYY, H:mm A");
+let seoulTime = moment().tz("Asia/Seoul").format("dddd, MMMM Do, YYYY, H:mm A");
+let taipeiTime = moment()
+  .tz("Asia/Taipei")
+  .format("dddd, MMMM Do, YYYY, H:mm A");
